3c9931c95967ab571a6dea5b39a28e83b8647788
younghearts <331912665@qq.com>
2020-11-03 15:41:19 +0800

    添加webpack配置文件

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/**** webpack.config.js  ***/
+
+// webpack 默认配置
+const path = require('path');
+
+module.exports = {
+    // 项目入口文件 支持 str | [] | {}
+    entry: path.resolve(__dirname, './src/index.js'),
+    // 项目出口 
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'mian.js'
+    },
+    // 打包环境 默认是生产环境 production
+    // 如果是开发环境 这里需要换成 development
+    // 接下来为了观察打包后的文件，使用 development
+    mode: 'development',
+    // 模块 这些选项决定了如何处理项目中的不同类型的模块。
+    module: {},
+    // 插件
+    plugins: [],
+    // 是否开启 source-map
+    // devtool: 'source-map'
+}
